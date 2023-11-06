c7d88952699dd677452ab3cf44317837a92d2612
hulefei <hulefei29@gmail.com>
2023-11-06 21:45:55 +0800

    【update】update main

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import torch
 # 检查CUDA是否可用
 if torch.cuda.is_available():
     print("CUDA is available! Training on GPU.")
+    print("GPU Device Name: ", torch.cuda.get_device_name(0))
+    print("GPU Device Count: ", torch.cuda.device_count())
 else:
     print("CUDA is not available. Training on CPU.")
