1125cc3385bdc90ba6ebcc0fb9c799232cdbe5b3
hulefei <hulefei29@gmail.com>
2023-11-09 16:38:33 +0800

    【update】更新 requiremnets.txt

--- cuda.py
+++ cuda.py
@@ -1,3 +1,4 @@
 import torch
+# pip3 install torch==2.1.0 torchvision==0.16.0  --index-url https://download.pytorch.org/whl/cu121
 print(f"cuda available: {torch.cuda.is_available()}")
 print(f"cuda version: {torch.version.cuda}")
